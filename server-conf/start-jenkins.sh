11244958ca0b40e7a5e99a7222ddbb8cc5f4eb1c
lunarbunny <tingwei1734@gmail.com>
2023-10-11 02:42:01 +0800

    Jenkins and dind docker commands

--- server-conf/start-jenkins.sh
+++ server-conf/start-jenkins.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+
+docker run --name jenkins-docker --rm --detach \
+  --privileged --network jenkins --network-alias docker \
+  --env DOCKER_TLS_CERTDIR=/certs \
+  --volume jenkins-docker-certs:/certs/client \
+  --volume jenkins-data:/var/jenkins_home \
+  --publish 2376:2376 \
+  docker:dind --storage-driver overlay2
+
 docker run --name jenkins --restart=on-failure --detach \
   --network jenkins \
   --env DOCKER_HOST=tcp://docker:2376 \
+  --env DOCKER_CERT_PATH=/certs/client \
+  --env DOCKER_TLS_VERIFY=1 \
   --env JENKINS_OPTS="--prefix=/jenkins" \
   --publish 8080:8080 \
   --volume jenkins-data:/var/jenkins_home \
