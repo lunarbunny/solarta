62060dfa107cc3b9fae72f37e302fcca5388cff1
unknown <2100888@sit.singaporetech.edu.sg>
2023-10-12 20:11:15 +0800

    Remove /api prefix for server endpoints

--- server/app/main.py
+++ server/app/main.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
+CORS(app, resources={r"*": {"origins": "http://localhost:3000"}})
 
 # Register NoSQL blueprints
 from blueprints.nosql.history_bp import history_bp
@@ -17,11 +17,11 @@ from blueprints.sql.music_bp import music_bp
 from blueprints.sql.playlist_bp import playlist_bp
 from blueprints.sql.user_bp import user_bp
 
-app.register_blueprint(album_bp, url_prefix="/api/album")
-app.register_blueprint(genre_bp, url_prefix="/api/genre")
-app.register_blueprint(music_bp, url_prefix="/api/music")
-app.register_blueprint(playlist_bp, url_prefix="/api/playlist")
-app.register_blueprint(user_bp, url_prefix="/api/user")
+app.register_blueprint(album_bp, url_prefix="/album")
+app.register_blueprint(genre_bp, url_prefix="/genre")
+app.register_blueprint(music_bp, url_prefix="/music")
+app.register_blueprint(playlist_bp, url_prefix="/playlist")
+app.register_blueprint(user_bp, url_prefix="/user")
 
 # Base endpoints
 @app.route("/")
