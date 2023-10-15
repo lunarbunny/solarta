34953ec169c5740e923d5589cf7bafb012969776
lunarbunny <tingwei1734@gmail.com>
2023-10-16 01:21:44 +0800

    Updated latest docker commands

--- server-conf/start-server.sh
+++ server-conf/start-server.sh
@@ -4,16 +4,19 @@
 docker network create --subnet=172.19.0.0/24 --gateway=172.19.0.254 solarta
 
 # Change the username/password out for the real one before running.
-docker run -d --network solarta --ip 172.19.0.1 -p 3306:3306 \
+docker run -d --name mariadb --network solarta --ip 172.19.0.1 -p 3306:3306 \
   --env MARIADB_USER=user --env MARIADB_PASSWORD=pass \
   --env MARIADB_ROOT_PASSWORD=root \
   --env MARIADB_DATABASE=solarta \
   --volume /var/lib/mysql:/var/lib/mysql \
-  --name mariadb mariadb:latest
+  mariadb:latest
 
 # SQLAlchemy Format: "mysql+pymysql://<username>:<password>@<host>/<db>"
-docker run -d --network solarta --ip 172.19.0.2 -p 5000:5000 \
-  --env SQLALCHEMY_DATABASE_URI="mysql+pymysql://user:pass@mariadb:3306/solarta" \
-  --env GOOGLE_APPLICATION_CREDENTIALS="/creds/serviceAccountKey.json" \
-  --volume ~/keys:/creds \
-  --name solarta solarta-api:latest
+docker run -d --name solarta --network solarta --ip 172.19.0.2 -p 5000:5000 \
+  --env SQLALCHEMY_DATABASE_URI=mysql+pymysql://user:pass@mariadb:3306/solarta \
+  --env GOOGLE_APPLICATION_CREDENTIALS=/creds/serviceAccountKey.json \
+  --volume /home/student51/keys:/creds \
+  solarta-api:latest
+
+# Single line version for jenkins docker run argument (omits --detach and image name)
+--name solarta --network solarta --ip 172.19.0.2 -p 5000:5000 --env SQLALCHEMY_DATABASE_URI=mysql+pymysql://user:pass@mariadb:3306/solarta --env GOOGLE_APPLICATION_CREDENTIALS=/creds/serviceAccountKey.json --volume /home/student51/keys:/creds
