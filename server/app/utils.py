c3f2cbb11c7bdf2556b5ffd1557f3c7e5594afed
Hina <44663281+nutshoekey@users.noreply.github.com>
2023-10-18 20:40:36 +0800

    created register endpoint
    missing write user to database

--- server/app/utils.py
+++ server/app/utils.py
@@ -1,4 +1,9 @@
+from argon2 import PasswordHasher
 import os
+import re
+
+email_regex = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+argon_hasher = PasswordHasher()
 
 def music_get_path(music_filename):
     return os.path.join("assets/music", music_filename)
@@ -12,3 +17,15 @@ def music_get_duration(music_filename):
     if file is not None:
         duration = file.info.time_secs
         return duration
+
+def is_email_valid(email):
+    return re.match(email_regex, email)
+
+def hash_password(password):
+    return argon_hasher.hash(password)
+
+def check_needs_rehash(hash):
+    return argon_hasher.check_needs_rehash(hash)
+
+def verify_password_hash(hash, password):
+    return argon_hasher.verify(hash, password)
