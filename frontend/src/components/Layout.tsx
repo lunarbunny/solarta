6e95f0ca0d39cb44f312636a79a973ec882c5c65
lunarbunny <tingwei1734@gmail.com>
2023-10-22 19:20:00 +0800

    Fix layout overflow

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -9,16 +9,16 @@ interface Props {
 
 const Layout: React.FC<Props> = ({ children }) => {
   return (
-    <Flex h="100vh" direction="column" bg="blackAlpha.500">
-      <Box as="main" flexGrow={1}>
-        <Flex flexDir="row" h="100%" w="100%">
+    <Flex as="main" direction="column" bg="blackAlpha.500" h="100vh">
+      <Box flexGrow={1}>
+        <Flex direction="row" h="100%">
           <Sidebar />
-          <Box overflowY="scroll" w="100%" h="100%">
+          <Box overflowY="scroll">
             {children}
           </Box>
         </Flex>
       </Box>
-      <Box boxShadow="0px 0px 5px 0px rgba(0,0,0,0.75)" w="100%" h="auto">
+      <Box boxShadow="0px 0px 5px 0px rgba(0,0,0,0.75)" w="100%">
         <MusicPlayer />
       </Box>
     </Flex>
