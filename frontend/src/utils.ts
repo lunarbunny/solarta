47bfc024524b2b7ce36b09fb0aee34b3e1ca0546
yucan-yu <2100888@sit.singaporetech.edu.sg>
2023-11-04 15:35:36 +0800

    Init CSRF tokens (without validation)

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -1,3 +1,5 @@
+/* Date utils */
+
 export function dateToYear(date: string): string {
   // Sun, 01 Jan 2023 00:00:00 GMT
   return date.split(" ")[3];
@@ -24,6 +26,8 @@ export function formatDate(date: Date): string {
   return `${year}-${month}-${day}`;
 }
 
+/* Validation utils */
+
 // For song names, album names
 export function validateName(text: string): boolean {
   return text.length >= 3 && text.length <= 64;
@@ -49,4 +53,59 @@ export function validatePwd(pwd: string, checkLength?: boolean): boolean {
 
 export function validateOTP(otp: string): boolean {
   return otp.length == 6 && !isNaN(Number(otp));
+}
+
+/* CSRF utils */
+import { API_URL } from "./types";
+
+async function getCsrfToken() {
+  const response = await fetch(`${API_URL}/csrf_token`);
+  const data = await response.json();
+  return data.token;
+}
+
+export async function postWithCsrfToken(endpoint: string, data: any): Promise<boolean> {
+  const csrfToken = await getCsrfToken();
+  const headers = {
+    'X-CSRFToken': csrfToken,
+    'Content-Type': 'application/json'
+  };
+
+  const response = await fetch(endpoint, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify(data)
+  });
+
+  return response.ok;
+}
+
+export async function putWithCsrfToken(endpoint: string, data: any): Promise<boolean> {
+  const csrfToken = await getCsrfToken();
+  const headers = {
+    'X-CSRFToken': csrfToken,
+    'Content-Type': 'application/json'
+  };
+
+  const response = await fetch(endpoint, {
+    method: 'PUT',
+    headers,
+    body: JSON.stringify(data)
+  });
+
+  return response.ok;
+}
+
+export async function deleteWithCsrfToken(endpoint: string): Promise<boolean> {
+  const csrfToken = await getCsrfToken();
+  const headers = {
+    'X-CSRFToken': csrfToken,
+  };
+
+  const response = await fetch(endpoint, {
+    method: 'DELETE',
+    headers
+  });
+
+  return response.ok;
 }
